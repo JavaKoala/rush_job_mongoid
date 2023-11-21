0f564e70b0477ebee1fb4e446bbb8c69fef40cd8
JavaKoala <javakoala1@gmail.com>
2023-11-21 16:27:34 -0500

    Add job class and arguments

--- app/models/rush_job_mongoid/rush_job.rb
+++ app/models/rush_job_mongoid/rush_job.rb
@@ -13,5 +13,24 @@ module RushJobMongoid
     field :failed_at,  type: Time
     field :last_error, type: String
     field :queue,      type: String
+
+    def job_class
+      job_data[:job_class]
+    end
+
+    def job_arguments
+      job_data[:arguments].presence || ''
+    end
+
+    private
+
+    def handler_hash
+      safe_yaml = handler.sub('!ruby/object:ActiveJob::QueueAdapters::DelayedJobAdapter::JobWrapper', '')
+      Psych.safe_load(safe_yaml, symbolize_names: true)
+    end
+
+    def job_data
+      handler_hash[:job_data]
+    end
   end
 end
--- test/models/rush_job_test.rb
+++ test/models/rush_job_test.rb
@@ -2,11 +2,23 @@ require 'test_helper'
 
 module RushJobMongoid
   class RushJobTest < ActiveSupport::TestCase
-    test 'job_arguments returns the arguments of the job' do
-      rush_job = RushJob.new
-      rush_job.handler = 'test handler'
+    setup do
+      @example_handler = "--- !ruby/object:ActiveJob::QueueAdapters::DelayedJobAdapter::JobWrapper\njob_data:\n  " \
+                         "job_class: TestHandler\n  arguments:\n  - arg1"
+    end
 
-      assert_equal rush_job.handler, 'test handler'
+    test 'job_class returns class of the job' do
+      rush_job = RushJob.new
+      rush_job.handler = @example_handler
+
+      assert_equal rush_job.job_class, 'TestHandler'
+    end
+
+    test 'job_arguments returns arguments of the job' do
+      rush_job = RushJob.new
+      rush_job.handler = @example_handler
+
+      assert_equal rush_job.job_arguments, ['arg1']
     end
   end
 end
