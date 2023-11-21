08844cc604a6ba01981a47ba5b4d7d7ed81f9a06
JavaKoala <javakoala1@gmail.com>
2023-11-21 16:03:01 -0500

    Remove indexes

--- app/models/rush_job_mongoid/rush_job.rb
+++ app/models/rush_job_mongoid/rush_job.rb
@@ -4,19 +4,14 @@ module RushJobMongoid
 
     store_in collection: 'delayed_backend_mongoid_jobs'
 
-    field :priority,    type: Integer, default: 0
-    field :attempts,    type: Integer, default: 0
-    field :handler,     type: String
-    field :run_at,      type: Time
-    field :locked_at,   type: Time
-    field :locked_by,   type: String
-    field :failed_at,   type: Time
-    field :last_error,  type: String
-    field :queue,       type: String
-
-    index locked_by: -1, priority: 1, run_at: 1
-    index queue: 1, locked_by: -1, priority: 1, run_at: 1
-    index failed_at: 1, locked_by: -1, priority: 1, run_at: 1
-    index failed_at: 1, queue: 1, locked_by: -1, priority: 1, run_at: 1
+    field :priority,   type: Integer, default: 0
+    field :attempts,   type: Integer, default: 0
+    field :handler,    type: String
+    field :run_at,     type: Time
+    field :locked_at,  type: Time
+    field :locked_by,  type: String
+    field :failed_at,  type: Time
+    field :last_error, type: String
+    field :queue,      type: String
   end
 end
