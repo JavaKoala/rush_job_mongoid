a05ff2dd10d3f8545d2560698ab7061fc64837df
JavaKoala <javakoala1@gmail.com>
2023-11-25 09:33:00 -0500

    Add many pages pagination

--- app/controllers/rush_job_mongoid/rush_jobs_controller.rb
+++ app/controllers/rush_job_mongoid/rush_jobs_controller.rb
@@ -2,6 +2,7 @@ module RushJobMongoid
   class RushJobsController < ApplicationController
     def index
       @page = params[:page]&.to_i || 1
+      @pages_count = RushJobMongoid::RushJob.pages_count
       @rush_jobs = RushJob.locked_by_desc.paginate(@page)
     end
   end
