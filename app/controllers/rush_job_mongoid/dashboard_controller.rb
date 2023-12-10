cd495a50434e27816cd026d8112abef2fcc75533
JavaKoala <javakoala1@gmail.com>
2023-12-10 11:43:35 -0500

    Add queue groups pagination

--- app/controllers/rush_job_mongoid/dashboard_controller.rb
+++ app/controllers/rush_job_mongoid/dashboard_controller.rb
@@ -1,7 +1,10 @@
 module RushJobMongoid
   class DashboardController < ApplicationController
     def index
-      @queue_groups = RushJob.queue_groups
+      @queue_groups_page = params[:queue_groups_page]&.to_i || 1
+      @rush_job_queue_groups = RushJob.queue_groups
+      @queue_groups_pages_count = RushJob.pages_count(@rush_job_queue_groups.count, 10)
+      @queue_groups = @rush_job_queue_groups[(@queue_groups_page - 1) * 10, 10]
     end
   end
 end
---
+++ test/system/rush_job_mongoid/dashboard_pagination_test.rb
@@ -0,0 +1,69 @@
+require 'application_system_test_case'
+
+module RushJobMongoid
+  class DashboardPaginationTest < ApplicationSystemTestCase
+    setup do
+      DatabaseCleaner.start
+    end
+
+    teardown do
+      DatabaseCleaner.clean
+    end
+
+    def create_jobs(number_of_jobs = 25)
+      number_of_jobs.times do |n|
+        RushJob.create(queue: "Queue #{n}", priority: n)
+      end
+    end
+
+    test 'No pagination for a single page' do
+      visit '/rush_job_mongoid'
+
+      assert_no_selector '.pagination'
+    end
+
+    test 'Queue group pagination for multiple pages' do
+      create_jobs
+      visit '/rush_job_mongoid'
+
+      assert_selector '.pagination'
+      assert_text 'Queue 0'
+      assert_text 'Queue 9'
+      assert_no_text 'Queue 10'
+      assert_no_text '...'
+      assert_link 'Previous'
+      assert_css '.disabled', text: 'Previous'
+      assert_link '1'
+      assert_css '.active', text: '1'
+      assert_link '2'
+      assert_no_css '.active', text: '2'
+    end
+
+    test 'Next queue group button' do
+      create_jobs
+      visit '/rush_job_mongoid'
+
+      click_link 'Next'
+      assert_text 'Queue 10'
+      assert_text 'Queue 19'
+      assert_no_text 'Queue 20'
+      assert_no_css '.disabled', text: 'Previous'
+      assert_link '1'
+      assert_no_css '.active', text: '1'
+      assert_link '2'
+      assert_css '.active', text: '2'
+    end
+
+    test 'Last queue group link' do
+      create_jobs
+      visit '/rush_job_mongoid'
+
+      click_link '3'
+      assert_text 'Queue 20'
+      assert_text 'Queue 24'
+      assert_no_css '.disabled', text: 'Previous'
+      assert_css '.active', text: '3'
+      assert_css '.disabled', text: 'Next'
+    end
+  end
+end
