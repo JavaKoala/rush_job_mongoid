5053c6426798d20eb5a972ee162d813667a9155d
JavaKoala <javakoala1@gmail.com>
2024-04-03 18:05:49 -0400

    Handle unsuccessful edits

--- app/controllers/rush_job_mongoid/rush_jobs_controller.rb
+++ app/controllers/rush_job_mongoid/rush_jobs_controller.rb
@@ -1,23 +1,31 @@
 module RushJobMongoid
   class RushJobsController < ApplicationController
+    before_action :find_job, only: %i[edit update]
+
     def index
       @pagination_presenter = PaginationPresenter.new(params[:page])
       @rush_jobs = RushJob.filter(filter_params).locked_by_desc.paginate(@pagination_presenter.page, 20)
     end
 
     def edit
-      flash[:warning] = t(:edit_warning)
-      @job = RushJob.find(params[:id])
+      flash.now[:warning] = t(:edit_warning)
     end
 
     def update
-      @job = RushJob.find(params[:id])
-      @job.update(update_params)
-
-      redirect_to rush_jobs_path(doc_id: @job.id)
+      if @job.update(update_params)
+        flash[:success] = t(:updated_job, job_id: @job.id)
+        redirect_to rush_jobs_path(doc_id: @job.id)
+      else
+        flash[:danger] = t(:unable_to_update, errors: @job.errors.full_messages.to_sentence)
+        render :edit, status: :unprocessable_entity
+      end
     end
 
     private
+
+    def find_job
+      @job = RushJob.find(params[:id])
+    end
 
     def filter_params
       params.permit(:doc_id, :priority, :attempts, :job_class, :arguments, :locked_by, :last_error, :queue)
--- test/controllers/rush_job_mongoid/rush_jobs_controller_test.rb
+++ test/controllers/rush_job_mongoid/rush_jobs_controller_test.rb
@@ -23,7 +23,7 @@ module RushJobMongoid
       get "/rush_job_mongoid/rush_jobs/#{job.id}/edit"
 
       assert_response :success
-      assert_select '.text-body', "Edit Job #{job.id}"
+      assert_equal 'Verify workers are stopped before editing. Minimal validations are applied.', flash[:warning]
     end
 
     test 'successful edit should redirect to index' do
@@ -31,7 +31,16 @@ module RushJobMongoid
       patch rush_job_url(job), params: { rush_job: { priority: 10 } }
 
       assert_redirected_to rush_jobs_path(doc_id: job.id)
+      assert_equal "Successfully updated job #{job.id}", flash[:success]
       assert_equal job.reload.priority, 10
+    end
+
+    test 'unsuccessful edit' do
+      job = RushJob.create
+      patch rush_job_url(job), params: { rush_job: { priority: '' } }
+
+      assert_response :unprocessable_entity
+      assert_equal "Unable to edit job. Priority can't be blank", flash[:danger]
     end
   end
 end
